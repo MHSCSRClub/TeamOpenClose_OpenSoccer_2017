a215b0992a6570e69a225f1bf200daeeb1a10de1
Angus Trau <angus.amtra5@gmail.com>
2017-06-22 16:14:36 +1000

    Init

---
+++ attack/attack.ino
@@ -0,0 +1,12 @@
+
+void setup() {
+	// put your setup code here, to run once:
+	Serial.begin(9600);
+	//while (!Serial.available()) {}
+	Serial.println('Hello world');
+}
+
+void loop() {
+	// put your main code here, to run repeatedly:
+  
+}
